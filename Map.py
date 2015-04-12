9d32b742e32d095071328151615d732160b97300
Greg <sunerg51@students.rowan.edu>
2015-04-11 23:00:40 -0400

    No one ever expects the A* inquisition

--- Map.py
+++ Map.py
@@ -30,18 +30,97 @@ class Map(object):
 	#Find out if something (type) is accessible from where it is. This includes range, so it's always
 	#one larger than reachable. possible refactoring to show that. Basically, this is for is X in my attack
 	#range?
-	def in_prox(self, source, type, range):
+	def in_prox(self, source,location, type, range):
 		pass
-	#returns something related to things that this unit is in proximity too. Remember, proximity doesn't mean
+	#Remember, proximity doesn't mean
 	#you can reach that square, it means that you can get within range of it, with specified range.
-	def proximity(self, source, type, range):
+	#list of things it can get in range of ie. distance + range
+	def proximity(self, source,location, type, range):
 		pass
-	#returns something. Probably a list of all squares a source can reach. maybe.
-	def reachable(self, source, end):
-		pass
-	#can thing in source move to ordered pair end in one move?
-	def is_reachable(self, start, end):
-		pass
+	#Returns list of squares in range of source
+	def reachable(self, source,location, end):
+		unitRange = source.getMovement()
+		squares = []
+		
+		#add squares in diamond range
+		for x in range (-unitRange, unitRange)
+			if(x<=unitRange)
+				h=x-unitRange
+			else
+				h=unitRange-x
+			for y in range (-h,h)
+				if((location[0]+x+1)<xDim & (location[1]+y+1])<yDim)
+					squares.append([location[0]+x+1,location[1]+y+1])
+		
+		inRange = []
+		#use A* to check with movement costs of terrain
+		for target in squares
+			path, cost = get_best_path(source, location, target)
+			if(cost<=unitRange)
+				inRange.append(target)
+		return inRange
+		
+		
+	#can thing in source move to ordered pair end in one move
+	def is_reachable(self, source, start, end):
+		path, range = get_best_path(source, start, end)
+		if(range <= source.getMovement())
+			return 1
+		else
+			return 0
+			
+	
+#example from online
+	
+	def get_best_path(self, source, current, goal)
+		frontier = PriorityQueue()
+		frontier.put(start, 0)
+		came_from = {}
+		cost_so_far = {}
+		came_from[start] = None
+		cost_so_far[start] = 0
+
+		while not frontier.empty():
+			current = frontier.get()
+
+			if current == goal:
+				#return the path
+				break
+		   
+			for next in graph.neighbors(current):
+				new_cost = cost_so_far[current] + graph.cost(next)
+				if next not in cost_so_far or new_cost < cost_so_far[next]:
+					cost_so_far[next] = new_cost
+					priority = new_cost + heuristic(goal, next)
+					frontier.put(next, priority)
+					came_from[next] = current
+		return came_from, cost_so_far			
+
+	#manhattan heuristic - total of difference of x and y	
+	def heuristic(self, goal, current)
+		x = abs(goal[0]-next[0])
+		y = abs(goal[1]-next[0])
+		return x+y
+	
+	#return 
+	def neighbors(self, current)
+		if(current[0]+1<xDim)
+			adj.append([current[0]+1,current[1]])
+		if(current[0]-1<xDim)
+			adj.append([current[0]-1,current[1]])
+		if(current[1]+1<yDim)
+			adj.append([current[0],current[1]+1])
+		if(current[1]-1]<yDim)
+			adj.append([current[0],current[1]-1])
+		
+		return adj
+		
+	#pull cost from terrain	
+	def cost(self, next)
+		x = next[0]
+		y = next[1]
+		return grid[x][y].cost
+
 	
 	#Parses the grid. Using the legend provided, it checks the map symbols against the terrain type
 	#file. it place the correct type of terrain in that square, and moves on.
